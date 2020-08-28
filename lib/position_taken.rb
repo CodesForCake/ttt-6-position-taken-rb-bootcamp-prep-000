f6149af13a86ee23833ed7efa173d6e0f21673c6
Mimi C <mina123fire@yahoo.com>
2020-08-28 19:11:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == nil || board[index] == ""
+  if board[index] == (" "||nil||"")
     return FALSE
-  elsif board[index] == "X"||board[index] == "O"
+  elsif board[index] == ("X"||"O")
     return TRUE
   end
 end
